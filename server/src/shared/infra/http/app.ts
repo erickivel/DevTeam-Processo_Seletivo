9592643ccb7d6cbfd072402f9c9ce0b6456c22ad
Eric Kivel <ericvkivel1@gmail.com>
2022-06-28 22:57:03 -0300

    docs: create user documentation

--- server/src/shared/infra/http/app.ts
+++ server/src/shared/infra/http/app.ts
@@ -1,14 +1,17 @@
 import "reflect-metadata";
+import swaggerUi from "swagger-ui-express";
+import express from "express";
 
 import "../../container";
-
-import express from "express";
+import swaggerDocument from "../../../swagger.json";
 
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(router);
 
