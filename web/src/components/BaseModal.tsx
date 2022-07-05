bfbd0b57373b336b558c00eccc68e7997e003d1c
Eric Kivel <ericvkivel1@gmail.com>
2022-07-05 11:00:03 -0300

    feat: modal component

---
+++ web/src/components/BaseModal.tsx
@@ -0,0 +1,40 @@
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface BaseModalProps {
+  title: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+export const BaseModal: React.FC<BaseModalProps> = ({ title, isOpen, onClose, children }) => {
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent w="100%" maxWidth={490} px="16" py="14">
+          <ModalHeader p="0">
+            <Text
+              mb="8"
+              alignSelf="flex-start"
+              fontFamily="Roboto"
+              color="orange.500"
+              fontSize="4xl"
+              fontWeight="medium"
+            >
+              {title}
+            </Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mt="-1" p="0">
+            {children}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+
+
+  );
+}
